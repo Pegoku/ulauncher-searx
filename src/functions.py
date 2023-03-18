03b5e89c0250a08c70062bb66f837e91441ccb58
Pegoku <81113533+Pegoku@users.noreply.github.com>
2023-03-18 16:26:57 +0100

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -5,7 +5,7 @@ from urllib.parse import urlencode
 
 SEARCH_URL = "https://search.brave.com/search?"
 SUGGESTION_URL = "https://duckduckgo.com/ac/?"    
-API_URL = "https://api.duckduckgo.com/?"
+API_URL = "https://searx.work/?"
 
 url = "https://docs.python.org/3.4/howto/urllib2.html"
 
